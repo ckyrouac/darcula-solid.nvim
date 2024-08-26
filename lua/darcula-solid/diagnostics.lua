d183684addc3a5e56e526dda7d5f78a35b1126ed
Chris Kyrouac <ckyrouac@redhat.com>
2024-08-26 09:54:44 -0400

    Cleanup diagnostics symbols

--- lua/darcula-solid/diagnostics.lua
+++ lua/darcula-solid/diagnostics.lua
@@ -2,7 +2,7 @@ vim.diagnostic.config({
   float = { border = "rounded" },
 })
 
-local signs = { Error = "󰅚", Warn = "󰀪", Hint = "💡", Info = "" }
+local signs = { Error = "󰅚", Warn = "󰀪", Hint = "💡", Info = "🛈" }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl })
